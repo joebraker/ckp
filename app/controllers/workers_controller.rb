e6fc8f18d5fe271ae40b18b028020ece2af0e434
Vadim A. Venediktov <install.vv@gmail.com>
2008-12-24 02:52:23 +0300

    New: added everything

---
+++ app/controllers/workers_controller.rb
@@ -0,0 +1,11 @@
+class WorkersController < ApplicationController
+  def index
+  end
+
+  def profile
+  end
+
+  def add_user
+  end
+
+end
---
+++ test/functional/workers_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class WorkersControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
