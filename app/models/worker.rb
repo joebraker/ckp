20676f11c62d912622f78323a5773be35d9abb24
Vadim A. Venediktov <install.vv@gmail.com>
2008-12-25 17:37:40 +0300

    New: new workers view designed

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -2,7 +2,7 @@ class Worker < ActiveRecord::Base
 	validates_presence_of :lastname
 	validates_uniqueness_of :lastname
 
-	DEGREE_TYPES = ["без степени", "канд. физ.-мат. наук", "д-р. физ.-мат. наук"]
+	DEGREE_TYPES = ["без степени", "кандидат", "доктор"]
 	RANK_TYPES = ["без звания", "доцент", "профессор"]
 	FACULTY_TYPES = ["физический", "химический", "биологический", "наук о материалах"]
 end
